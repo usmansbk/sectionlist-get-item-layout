131f1806b3b13c292d685adbbf0b181c1660ffe4
Usman Suleiman <usmansbk@gmail.com>
2019-06-03 09:07:41 +0100

    Patch

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ const getSectionItemLayout = ({
 	listHeaderHeight = 0,
 	listFooterHeight = 0,
 }) => (sections, index) => {
-	let length = 0, offset = 0, currentIndex = 0;
+	let length = listHeaderHeight, offset = 0, currentIndex = 0;
 	while (currentIndex < index) {
 		offset += length;
-		length = currentIndex > 0 ? listFooterHeight : listHeaderHeight;
+		if (currentIndex > 0)  length = listFooterHeight;
 		currentIndex++;
 		const sectionsLength = sections.length;
 		for (let sectionIndex = 0; ((sectionIndex < sectionsLength) && (currentIndex < index)); sectionIndex++) {
